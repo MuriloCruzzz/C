58916c79a24c02dd12dd0b4948107883db3d1335
ANDREW <102931168+AndrewBait@users.noreply.github.com>
2022-08-19 14:52:21 -0300

    Update EX05.c

--- LISTA 1- ED/EX05.c
+++ LISTA 1- ED/EX05.c
@@ -11,16 +11,16 @@ int main(void) {
   scanf("%i", &num);
 
   if (num % 4 == 0 && num % 5 == 0)
-    printf("O numero %i é divizível por 4 e por 5\n", num);
+    printf("O numero %i é divisível por 4 e por 5\n", num);
   
   else if(num % 4 == 0 )
-    printf("O numero %i é divizível por 4", num);
+    printf("O numero %i é divisível por 4", num);
   
   else if(num % 5 == 0)
-    printf("O numero %i é divizível po 5", num);
+    printf("O numero %i é divisível po 5", num);
 
   else
-    printf("%i não é divizível por 4 e nem po 5", num);
+    printf("%i não é divisível por 4 e nem po 5", num);
     
   return 0;
 }
