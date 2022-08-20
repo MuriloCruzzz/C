a64398da98a5c432fd4fbaaf95d5d997fb7b8e28
ANDREW <102931168+AndrewBait@users.noreply.github.com>
2022-08-20 11:20:05 -0300

    Update MENU (em andamento).c

--- LISTA 1- ED/MENU (em andamento).c
+++ LISTA 1- ED/MENU (em andamento).c
@@ -16,13 +16,22 @@ int ler_opcao(){
       scanf("  %i", &op);
       switch(op){
           case 1:
+            break;
           case 2:
+            break;
           case 3:
+            break;
           case 4:
+            break;
           case 5:
+            break;
           case 6:
+            break;
           case 7:
+            break;
           case 8:
+            exit(0);
+            break;
               return op;
           default:
               printf("\n\nOPÇÃO INVÁLIDA!!\n\nSelecione o numero corresponde ao exercicio desejado\n\n");
@@ -37,7 +46,7 @@ int ler_opcao(){
     int op;
     menu();
     op = ler_opcao();
-    printf("O exercicio escolhido foi %i", op);
+    printf("O usuário escolheu o exercicio %i", op);
     
 
     
