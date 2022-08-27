102739e3576b13afab26a84118533034a6b2fc4d
ANDREW <102931168+AndrewBait@users.noreply.github.com>
2022-08-26 22:30:50 -0300

    Add files via upload

---
+++ LISTA 2 - ED/EX002 - 1DIV1 + 3DIV2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+  float i, j;
+  
+  for(i = 1, j=1 ; i <= 50, j<= 90; i++, j++){
+    printf("total: %f ", (i/2)+ (j/3));
+    }
+return 0;
+
+  
+}
+  
+
+
+
+
+
+
