06c2364b44877552ed3c80017cd52d2f4393af6e
ANDREW <102931168+AndrewBait@users.noreply.github.com>
2022-08-27 10:55:52 -0300

    Update EX002 - 1DIV1 + 3DIV2.c

--- LISTA 2 - ED/EX002 - 1DIV1 + 3DIV2.c
+++ LISTA 2 - ED/EX002 - 1DIV1 + 3DIV2.c
@@ -1,15 +1,52 @@
 #include <stdio.h>
 
-int main(){
-  float i, j;
+int main(void) {
+  float i, j, r, soma;
+for(i = 1, j = 1; /*i <= 99,*/ j <= 50; i = i +2, j = j+1){
+    r = i / j;
+    printf("%.3f\n", r);
+    soma = soma + r;
+    
+  }
+  printf("\nTOTAL= %.3f", soma);
   
-  for(i = 1, j=1 ; i <= 50, j<= 90; i++, j++){
-    printf("total: %f ", (i/2)+ (j/3));
-    }
-return 0;
+  return 0;
+  }
 
-  
-}
+
+
+/*float i = 1, j = 1,  r = 0;
+  for(j = 1; i <= 50; i++, i + 2){
+  r += (i / j);
+  }
+  printf("%.f", r);*/
+
+
+
+
+/*int main(void) {
+  float i, j, r;
+for(i = 1, j = 1; i <= 99, j <= 50; (i = i +2, j = j + 1)){
+    //printf("%.f/%.f\n", i, j);
+    r = i / j;
+    printf("%f\n", r);
+  }
+  return 0;
+  }*/
+
+
+
+
+/*for(i = 1, j = 1; i <= 50; i++){
+    j = j +2;
+    r = r + ( j / i);  */
+
+
+/*for(i = 1, j = 1; i <= 99, j <= 50; (i = i +2, j = j + 1)){
+    //printf("%.f/%.f\n", i, j);
+    r = (i / j);
+    printf("%f\n", r);
+  }*/
   
 
 
