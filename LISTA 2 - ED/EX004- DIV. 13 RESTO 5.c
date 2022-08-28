6f6ce288264004125a1920cf14ca26f1b3b2f6c2
ANDREW <102931168+AndrewBait@users.noreply.github.com>
2022-08-28 19:56:18 -0300

    Update EX004- DIV. 13 RESTO 5.c

--- LISTA 2 - ED/EX004- DIV. 13 RESTO 5.c
+++ LISTA 2 - ED/EX004- DIV. 13 RESTO 5.c
@@ -1,7 +1,7 @@
 #import <stdio.h>
 
 void main(){
-    int n1, div =13, i;
+    int n1, div =13;
     for (n1 = 1000 ; n1 <=1999; n1++){
         if( n1 % div == 5 )
         printf("numeros divisiveis por 13 com resto 5: %i\n ",n1);
