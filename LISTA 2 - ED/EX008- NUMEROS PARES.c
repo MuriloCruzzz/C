7fc23a26d27b09c0d95f8a74ffe81486ac4aa74d
ANDREW <102931168+AndrewBait@users.noreply.github.com>
2022-08-29 00:10:02 -0300

    Add files via upload

---
+++ LISTA 2 - ED/EX008- NUMEROS PARES.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*void main(){
+    int n1, fim, i= 1;
+    printf("Defina o limie de numeros: ");
+    scanf("%i", &fim);
+    while( i < fim + 1){
+        printf("Digite o %iº valor: ", n1);
+        scanf("%i", &n1);
+        if (n1% 4 == 0|| n1 % 7 == 0);
+        printf("este numero é divisivel");
+    i++;
+    }
+}
+
+
+
+//
