6bf262dd16c007707391d174caf03bf2c6a57e26
ANDREW <102931168+AndrewBait@users.noreply.github.com>
2022-08-29 11:46:39 -0300

    Add files via upload

---
+++ LISTA 2 - ED/EX009 - ALTURA ANUAL.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main(){
+  int ano = 0;
+  float j = 1.35, r = 1.07;
+  
+  while(r < j){  
+  j = j + 0.02;
+  r = r + 0.03;
+  ano++;
+  }
+  printf("Demorará %d anos para que Josevaldo ser maior que Rafonildo", ano);
+  }
