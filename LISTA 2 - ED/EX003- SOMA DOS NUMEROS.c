3b67b8c4821bace2500a14e3ea3d66a825c422b7
Murilo Cruz <92764547+MuriloCruzzz@users.noreply.github.com>
2022-09-01 22:24:45 -0300

    murilocomit.c

--- LISTA 2 - ED/EX003- SOMA DOS NUMEROS.c
+++ LISTA 2 - ED/EX003- SOMA DOS NUMEROS.c
@@ -21,3 +21,36 @@ int main(void) {
   }
   return 0;
 }
+
+
+//----------------------murilo------------//
+/* //#include<stdio.h>
+
+int main(){
+
+
+    int numero_1 = 1, numero_2 = 1, resultado_soma, i;
+
+    printf("Entre com dois numeros para somá-los, só não entre com 0 [zero] \npois fechará o programa !!!\n");
+
+    for (i=0;; i++){
+        printf("Entre com um numero: ");
+        scanf("%d", &numero_1);
+        if (numero_1 != 0 && numero_2 != 0){
+        printf("Entre com o outro numero: ");
+        scanf("%d", &numero_2);
+        }
+        else {
+            printf("Você digitou 0 [ZERO] - Sistema fechou!");
+            return 0;
+        }
+        if (numero_1 != 0 && numero_2 != 0){
+        resultado_soma = (numero_1 + numero_2);
+        }
+        else {
+            printf("Você digitou 0 [ZERO] - Sistema fechou!");
+            return 0;
+        }
+        printf("A soma de %d + %d e = %d \n", numero_1,numero_2, resultado_soma);
+        }
+} */
